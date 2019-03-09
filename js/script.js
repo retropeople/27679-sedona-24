b063a4c110f896d1e65b53c3d6fed9598fd5dcac
retropeople <44552700+retropeople@users.noreply.github.com>
2019-03-09 17:55:27 +0100

    Добавляет правки разметки. стилей и скриптов

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 var toggler = document.querySelector(".hotels-search-toggler");
 var formContainer = document.querySelector(".form-container");
+var form = formContainer.querySelector(".booking-form");
+var arrivalDate = form.querySelector("[name=arrival_date]");
+var departureDate = form.querySelector("[name=departure_date]");
+var adultsValue = form.querySelector("[name=adults]");
+var kidsValue = form.querySelector("[name=kids]");
 
   toggler.addEventListener("click", function (evt) {
     evt.preventDefault();
     formContainer.classList.toggle("form-container-hidden");
+    formContainer.classList.remove("form-error");
   });
 
   document.addEventListener('DOMContentLoaded', function() {
      formContainer.classList.add("form-container-hidden");
   }, false);
+
+  form.addEventListener("submit", function (evt) {
+    if (!arrivalDate.value || !departureDate.value || !adultsValue.value || !kidsValue.value) {
+      evt.preventDefault();
+      formContainer.classList.remove("form-error");
+      formContainer.offsetWidth = formContainer.offsetWidth;
+      formContainer.classList.add("form-error");
+    }
+  })
